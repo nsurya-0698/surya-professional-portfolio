a4f1137bfab70ca4ac6158f24474e08325d612d2
nsurya-0698 <nsuryateja1997@gmail.com>
2025-02-16 15:50:47 -0600

    setting gh-pages

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import './index.css'
 const Footer = () => {
     return(
 
-            <p className="credit">Built by Surya Teja Nammi.</p>
+            <p className="credit">Built by Surya Teja Nammi</p>
     )
 }
 
